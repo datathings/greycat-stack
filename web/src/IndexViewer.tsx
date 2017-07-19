090fb7b04feb78e0c27d0bdda133c2fca0cd6f2f
Francois Fouquet <fouquet.f@gmail.com>
2017-07-19 17:07:07 +0200

    another try

--- web/src/IndexViewer.tsx
+++ web/src/IndexViewer.tsx
@@ -1,43 +1,34 @@
 import * as React from 'react';
-import { Node, Graph, NodeIndex } from 'greycat';
+import {Node, Graph, NodeIndex} from 'greycat';
 
 let ReactJson = require('react-json-view').default;
-import { bind } from 'decko';
+import {bind} from 'decko';
 
 class IndexViewer
     extends React.Component<{ graph: Graph, indexName: string },
-    { indexNode?: NodeIndex, nodes: Array<Node>, listeningID?: number }> {
+        { indexNode?: NodeIndex, nodes: Array<Node>, listeningID?: number }> {
 
     constructor(props: { graph: Graph, indexName: string }) {
         super(props);
-        this.state = { nodes: [] };
+        this.state = {nodes: []};
     }
 
     componentWillMount() {
-        let onIndexUpdate = this.onIndexUpdate;
-        this.props.graph.index(0, new Date().getTime(), this.props.indexName, onIndexUpdate);
-    }
-
-    @bind
-    onIndexUpdate(newIndex: NodeIndex) {
-        let updateState = this.updateState;
-        newIndex.findFrom((resolvedNodes: Array<Node>) => updateState({ indexNode: newIndex, nodes: resolvedNodes }));
+        this.props.graph.index(0, new Date().getTime(), this.props.indexName, (indexNode: NodeIndex) => {
+            var listeningID = indexNode.listen(() => indexNode.travelInTime(new Date().getTime(), (newIndex: NodeIndex)=>{
+                newIndex.findFrom((resolvedNodes: Array<Node>) => this.updateState({indexNode: newIndex, nodes: resolvedNodes, listeningID:this.state.listeningID}));
+            }));
+            indexNode.findFrom((resolvedNodes: Array<Node>) => this.updateState({indexNode: indexNode, nodes: resolvedNodes, listeningID:listeningID}));
+        });
     }
 
     componentWillUnmount() {
-        this.updateState({ nodes: [] });
+        this.updateState({nodes: []});
     }
 
     @bind
     updateState(newState: { indexNode?: NodeIndex, nodes: Array<Node>, listeningID?: number }) {
         let prevState = this.state;
-        let onIndexUpdate = this.onIndexUpdate;
-        if (prevState.listeningID === undefined && newState.indexNode !== undefined) {
-            let newIndex: NodeIndex = newState.indexNode;
-            newState.listeningID = newIndex.listen(() => newIndex.travelInTime(new Date().getTime(), onIndexUpdate));
-        } else {
-            newState.listeningID = prevState.listeningID;
-        }
         this.setState(newState, function () {
             if (prevState.indexNode !== undefined) {
                 if (prevState.listeningID !== undefined && newState.listeningID === undefined) {
@@ -50,7 +41,7 @@ class IndexViewer
     }
 
     render() {
-        return <ReactJson src={this.state.nodes.map(node => JSON.parse(node.toString())).slice(-10)} />;
+        return <ReactJson src={this.state.nodes.map(node => JSON.parse(node.toString())).slice(-10)}/>;
     }
 }
 
