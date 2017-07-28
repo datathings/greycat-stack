d04beb38723eace41e50f858aa2104a542700499
Francois Fouquet <fouquet.f@gmail.com>
2017-07-28 14:36:53 +0200

    update :-)

--- server/src/main/java/greycat/stack/Server.java
+++ server/src/main/java/greycat/stack/Server.java
@@ -4,6 +4,7 @@ import greycat.Graph;
 import greycat.GraphBuilder;
 import greycat.rocksdb.RocksDBStorage;
 import greycat.websocket.WSServer;
+import greycat.websocket.WSSharedServer;
 import model.ModelPlugin;
 
 public class Server {
@@ -13,7 +14,7 @@ public class Server {
         //Build Graph
         Graph graph = GraphBuilder.newBuilder().withStorage(new RocksDBStorage("data")).withPlugin(new ModelPlugin()).build();
         //Expose to WebSocket port
-        WSServer.attach(graph, port);
+        WSSharedServer.attach(graph, port);
         //Let's start
         graph.connect(connectionResult -> {
             System.out.println("GreyCat Temporal Graph Server Started on port: " + port);
